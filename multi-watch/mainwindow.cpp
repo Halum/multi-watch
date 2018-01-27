0a62661f72e04ecdce63248b0e645c65b70d9704
Sajjad Hossain <my.lost.word@gmail.com>
2018-01-28 02:31:08 +0600

    #11 - Add working digital clock

--- multi-watch/mainwindow.cpp
+++ multi-watch/mainwindow.cpp
@@ -1,14 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "digitalclock.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+  QMainWindow(parent),
+  ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  this->clock = new DigitalClock(this);
+  clock->init(this->ui->digitalClock);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
