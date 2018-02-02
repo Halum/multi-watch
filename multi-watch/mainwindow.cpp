07fd008b877fecb6b5e3f2f31da69f7a51736abf
Sajjad Hossain <my.lost.word@gmail.com>
2018-02-03 02:58:51 +0600

    #40 - Add demo logs to see if numpad works

--- multi-watch/mainwindow.cpp
+++ multi-watch/mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_mainwindow.h"
 #include "digitalclock.h"
 #include "settingmanager.h"
+#include "numpad.h"
+
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
@@ -9,19 +12,24 @@ MainWindow::MainWindow(QWidget *parent) :
   ui->setupUi(this);
   ui->changeTimeFormatBtn->setCheckable(true);
 
+  this->numpad = new Numpad(parent);
+  this->ui->timerTab->layout()->addWidget(this->numpad);
+
   this->clock = new DigitalClock(this);
   clock->init(this->ui->digitalClock, ui->changeTimeFormatBtn);
 
   this->settingManager = new SettingManager();
   this->settingManager->loadTabOrder(ui->tabWidget);
-  ui->tabWidget->setCurrentIndex(0);
 
   this->disableTabsForRelease();
+
+  connect(this->numpad, &Numpad::numberPressed,
+          this, &MainWindow::input);
 }
 
 void MainWindow::disableTabsForRelease(){
   this->ui->tabWidget->setTabEnabled(1, false);
-  this->ui->tabWidget->setTabEnabled(2, false);
+//  this->ui->tabWidget->setTabEnabled(2, false);
   this->ui->tabWidget->setTabEnabled(3, false);
 }
 
@@ -29,5 +37,10 @@ MainWindow::~MainWindow()
 {
   this->settingManager->saveTabOrder(ui->tabWidget);
   delete ui;
+}
+
+// this is a demo method to show that numpad works
+void MainWindow::input(int num) {
+  qDebug() << num;
 }
 
