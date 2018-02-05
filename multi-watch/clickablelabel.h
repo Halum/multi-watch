620e5f8571a9b58139cd1712f472c658e4625166
Sajjad Hossain <my.lost.word@gmail.com>
2018-02-06 00:41:03 +0600

    #45 - Add clickable QLabel widget into project

---
+++ multi-watch/clickablelabel.h
@@ -0,0 +1,23 @@
+#ifndef CLICKABLELABEL_H
+#define CLICKABLELABEL_H
+
+#include <QLabel>
+
+class QWidget;
+class QMouseEvent;
+
+class ClickableLabel : public QLabel
+{
+  Q_OBJECT
+public:
+  explicit ClickableLabel(QWidget* parent = Q_NULLPTR);
+  ~ClickableLabel();
+
+signals:
+  void clicked();
+
+protected:
+  void mousePressEvent(QMouseEvent* event);
+};
+
+#endif // CLICKABLELABEL_H
